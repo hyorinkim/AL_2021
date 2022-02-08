886f0a8d37936d532b4682dbb38b020f35a5ec88
hyorinkim <gyfls7748@gmail.com>
2022-02-08 18:29:43 +0900

    sys.stdin.readline으로 입력받기 list값 for문으로 출력하기

--- 220120/sort2.py
+++ 220120/sort2.py
@@ -1,8 +1,10 @@
-N=int(input())
+import sys
+N=int(sys.stdin.readline())
 list1=list()
 for i in range(N):
-    list1.append(int(input()))
+    list1.append(int(sys.stdin.readline()))
 list2=sorted(list1)
-print(list2)
+for data in (list2):
+    print(data)
 objrev=reversed(list1)
 print(list(objrev))
