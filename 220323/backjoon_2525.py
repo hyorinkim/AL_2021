5a9a9c0cc385f3af9f42a5d47b93505a9fc69d1f
hyorinkim <gyfls7748@gmail.com>
2022-03-23 16:03:26 +0900

    short code 첨가
    open(0).read 사용

--- 220323/backjoon_2525.py
+++ 220323/backjoon_2525.py
@@ -6,3 +6,8 @@ recurrent[1]=(recurrent[1]+running)%60
 if recurrent[0]>=24:
     recurrent[0]%=24
 print(recurrent[0],recurrent[1])
+
+#short code
+h,m,t=map(int,open(0).read().split())#open(0)는 파일 객체를 반환, read는 파일 전체의 내용을 하나의 문자열로 읽어 온다.
+m+=t
+print((h+m//60)%24,m%60)
