31be9629e25b9a9fc0f0a307b0c45dc130557103
hyorinkim <gyfls7748@gmail.com>
2022-03-23 15:57:06 +0900

    입력을 바꿈, 계산 순서 바꿈, 큰 값이 분 단위로 들어왔을 때 고려함
     recurrent[1]값이 바뀌기 전에 recurrent[0]값을 구함

--- 220323/backjoon_2525.py
+++ 220323/backjoon_2525.py
@@ -1,7 +1,8 @@
-recurrent=list(map(int,input().split()))
-running=int(input())
+time=[input()for __ in range(2) ]
+recurrent=list(map(int,time[0].split()))
+running=int(time[1])
+recurrent[0]+=int((recurrent[1]+running)/60)
 recurrent[1]=(recurrent[1]+running)%60
-recurrent[0]+=(recurrent[1]+running)/60
-if recurrent[0]==24:
-    recurrent[0]=0
-print("%d %d",recurrent[0],recurrent[1])
+if recurrent[0]>=24:
+    recurrent[0]%=24
+print(recurrent[0],recurrent[1])
