af654c3675bef6366a2610a95f6a2f0a05b27e1d
hyorinkim <gyfls7748@gmail.com>
2022-07-21 14:32:00 +0900

    7월12일 python 소스코드 time재는 코드

---
+++ 220712/time.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+start_time = time.time()#측정시작
+
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    #프로그램 소스코드
+    end_time= time.time()
+    print("time :", end_time-start_time)#수행시간 출력
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
